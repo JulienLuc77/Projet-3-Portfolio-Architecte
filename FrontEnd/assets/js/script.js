75d9a36b6ddb4e25268c0d05737cce121222d158
Julien <julien-luc77@gmail.com>
2023-07-25 14:55:14 +0200

    Ajout en Javascript de la modal - Retrait des boutons filtres llors de l'authentification correcte - Ajout des boutons modifier lors du login - Modification du CSS conformément à la maquette - Ajout du bouton logout lorsque l'authentification est faite en remplacement du login hors connexion - Mise en place du token dans le localstorage avec stockage ok + fetch - Connexion opérationnelle avec le compte test

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -108,3 +108,119 @@ fetch('http://localhost:5678/api/categories')
     });
   })
   .catch(error => console.error('Erreur lors de la récupération des données :', error));
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const loginButton = document.getElementById("loginButton");
+    loginButton.addEventListener("click", function (event) {
+        event.preventDefault(); 
+        loginUser();
+    });
+});
+
+function loginUser() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const validEmail = "sophie.bluel@test.tld";
+    const validPassword = "SOphie";
+
+    if (email === validEmail && password === validPassword) {
+        
+        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4";
+        localStorage.setItem("userToken", token);
+
+        
+        window.location.href = "./index.html?authenticated=true";
+    } else {
+        
+        alert("Identifiant ou mot de passe incorrect.");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+ 
+  const urlParams = new URLSearchParams(window.location.search);
+  const isAuthenticated = urlParams.get('authenticated');
+
+ 
+  const token = localStorage.getItem("userToken");
+  if (token && isAuthenticated !== "true") {
+     
+      window.location.href = "./index.html?authenticated=true";
+  }
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const logoutButton = document.getElementById("logoutButton");
+  logoutButton.addEventListener("click", function (event) {
+      event.preventDefault(); 
+
+      
+      localStorage.removeItem("userToken");
+
+      
+      window.location.href = "./index2.html";
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const loginListItem = document.getElementById("loginListItem");
+  const logoutListItem = document.getElementById("logoutListItem");
+  const modifyImageBtn = document.getElementById("modifyImageBtn");
+  const modifyProjectsBtn = document.getElementById("modifyProjectsBtn");
+  const modal = document.getElementById("modal");
+  const closeBtn = document.querySelector(".close-btn");
+  const filterButtons = document.getElementById("menu-categories"); // Récupérer le conteneur des boutons filtres
+
+  const token = localStorage.getItem("userToken");
+
+  if (token) {
+      
+      loginListItem.style.display = "none";
+      logoutListItem.style.display = "block";
+
+     
+      modifyImageBtn.classList.remove("hide-btn");
+      modifyProjectsBtn.classList.remove("hide-btn");
+
+      
+      filterButtons.style.display = "none";
+  } else {
+      
+      loginListItem.style.display = "block";
+      logoutListItem.style.display = "none";
+
+      
+      modifyImageBtn.classList.add("hide-btn");
+      modifyProjectsBtn.classList.add("hide-btn");
+
+   
+      filterButtons.style.display = "block";
+  }
+
+  logoutButton.addEventListener("click", function (event) {
+      event.preventDefault(); 
+
+      
+      localStorage.removeItem("userToken");
+
+      
+      window.location.href = "./index2.html";
+  });
+
+  modifyImageBtn.addEventListener("click", function () {
+      modal.style.display = "block";
+  });
+
+  modifyProjectsBtn.addEventListener("click", function () {
+      modal.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", function () {
+      modal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+      if (event.target === modal) {
+          modal.style.display = "none";
+      }
+  });
+});
