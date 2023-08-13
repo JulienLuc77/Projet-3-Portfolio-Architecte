26ea1cf1c5f97f55b1e621d81fffc8324966b610
Julien <julien-luc77@gmail.com>
2023-08-13 11:40:45 +0200

    Modification de post.js pour mise en fonctionnalité de la requete post ajout de nouveau travail

--- FrontEnd/assets/js/post.js
+++ FrontEnd/assets/js/post.js
@@ -1,46 +1,50 @@
 const addButton = document.getElementById('valider-button');
 const photoForm = document.getElementById('photo-form');
-addButton.addEventListener('submit', function (e) {
+addButton.addEventListener('click', function (e) {
   e.preventDefault();
-  
+
   const title = document.getElementById('photo-title').value;
   const category = document.getElementById('photo-description').value;
+  let categoryId;
+
   const imageFile = document.getElementById('photo-input').files[0];
   const userId = localStorage.getItem('userId');
 
+  switch (category) {
+    case "Objets":
+      categoryId = 1;
+      break;
+  
+    case "Appartements":
+      categoryId = 2;
+      break;
+
+    case "Hôtels & restaurants":
+      categoryId = 3;
+      break;
+
+    default:
+      categoryId = 0;
+      break;
+  }
 
   const formData = new FormData();
   formData.append('image', imageFile); 
   formData.append('title', title); 
-  formData.append('category', category);
-  formData.append('userId', userId);
+  formData.append('category', categoryId);
 
   fetch('http://localhost:5678/api/works', {
     method: 'POST',
     headers: {
-      'accept':'application/json',
       'Authorization': `Bearer ${localStorage.getItem('userToken')}`
     },
     body: formData
   })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Erreur lors de l\'ajout du travail');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Travail ajouté avec succès:', data);
-      
-      fetchTravaux();
-
-      return false();
-    })
-    .catch(error => {
-      console.error('Erreur:', error);
-      
-      
-      const errorMessage = document.getElementById('error-message');
-      errorMessage.textContent = 'Erreur lors de l\'ajout du travail. Veuillez réessayer.';
-    });
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 });
